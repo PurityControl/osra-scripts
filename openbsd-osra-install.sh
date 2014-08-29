d57d8add435932ff55d6eb00edb51e00cf8375a1
Craig Ferry <PurityControl@users.noreply.github.com>
2014-08-29 12:36:12 +0100

    added openbsd install script

---
+++ openbsd-osra-install.sh
@@ -0,0 +1,38 @@
+# TO DO implement sudo in the script
+# TO DO install bash and make default shell
+
+# install all required packages
+sudo pkg_add -I git postgresql-server bash node libxml2 libxslt
+
+sudo -u _postgresql mkdir /var/postgresql/data
+sudo -u _postgresql initdb -D /var/postgresql/data -U postgres -E UTF8
+sudo sh -c "echo 'pkg_scripts="postgresql"' >> /etc/rc.conf.local"
+sudo /etc/rc.d/postgresql start
+
+
+#install rvm
+\curl -sSL https://get.rvm.io | bash -s stable
+. ~/.rvm/scripts/rvm
+rvm requirements run
+
+#clone osra and cd into it
+git clone https://github.com/AgileVentures/osra.git
+cd osra
+
+# install ruby version needed for osra
+ruby_ver=`grep ^ruby Gemfile | cut -d ' ' -f 2 | tr -d "'"`
+rvm install ruby-$ruby_ver
+
+#install all the gems
+rvm use $ruby_ver
+gem install bundler
+bundle config build.nokogiri --use-system-libraries
+bundle install --without production
+
+# setup the osra database
+bundle exec rake db:create
+bundle exec rake db:schema:load
+bundle exec rake db:seed
+bundle exec rake db:test:prepare
+
+
