68ce3579b852b5a743f1e3c1fb3e77f014aeac93
Craig Ferry <PurityControl@users.noreply.github.com>
2014-09-12 18:25:36 +0100

    added vagrant file and provisioning script for easy vagrant setup
    vagrant setup of ubuntu64 added

---
+++ vagrant/ubuntu64/vagrant-trusty64-osra.sh
@@ -0,0 +1,46 @@
+#ask for the sudo password at the start and immediately update the package library
+echo 'about to ask for the sudo password so the script can install some things'
+sudo echo 'thankyou continuing with the script...'
+sudo apt-get update
+
+#download and install rvm
+\curl -sSL https://get.rvm.io | bash -s stable
+
+# load rvm on login
+echo '' >> ~/.bash_profile
+echo '#source profile for rvm' >> ~/.bash_profile 
+echo 'source ~/.profile' >> ~/.bash_profile 
+
+#load rvm to run now
+. ~/.bash_profile
+
+# install all packages needed to let rvm do its job
+rvm requirements run
+
+# install other necessary packages
+sudo apt-get install -y  git postgresql-9.3 libpq-dev nodejs nodejs-legacy npm \
+     qt4-dev-tools libqt4-dev libqt4-core libqt4-gui xvfb
+sudo npm install -g phantomjs
+
+#setup postgres
+sudo cp /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.orig
+sudo sed -i -e 's/local\s\+all\s\+postgres\s\+peer/local all postgres trust/g' /etc/postgresql/9.3/main/pg_hba.conf 
+sudo service postgresql reload
+
+#clone osra and cd into it
+git clone https://github.com/AgileVentures/osra.git /vagrant/osra
+cd /vagrant/osra
+
+# install ruby version needed for osra
+ruby_ver=`grep ^ruby Gemfile | cut -d ' ' -f 2 | tr -d "'"`
+rvm install ruby-$ruby_ver
+
+#install all the gems
+rvm use $ruby_ver
+gem install bundler
+bundle install --without production
+
+# setup the osra database
+bundle exec rake db:setup
+bundle exec rake db:test:prepare
+
