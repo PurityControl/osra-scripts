d9e15f8a04596e0e5d4e23397f7bffaf88bada12
Craig Ferry <PurityControl@users.noreply.github.com>
2014-12-15 07:58:14 +0000

    add rvm gpg key to standalone ubuntu provisioning script

--- ubuntu-osra-install.sh
+++ ubuntu-osra-install.sh
@@ -3,6 +3,7 @@ echo 'about to ask for the sudo password so the script can install some things'
 sudo echo 'thankyou continuing with the script...'
 
 #download and install rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
 # load rvm on login
